df068f1ead0d16d090ff79b0527cdce6611c5632
LittleBlue418 <hollyathomas88@gmail.com>
2020-05-28 15:33:30 +0200

    POST new recipe end point

--- code/models/recipes.py
+++ code/models/recipes.py
@@ -7,3 +7,41 @@ class RecipesModel():
             key: str(value) if key == '_id' else value
             for key, value in obj.items()
         }
+
+    @classmethod
+    def find_by_name(cls, name):
+        return mongo.db.recipes.find_one({"name": name})
+
+    @classmethod
+    def find_recipe_by_id(cls, _id):
+        return mongo.db.recipes.find_one({"_id": ObjectId(_id)})
+
+    @classmethod
+    def find_ingredient_by_id(cls, _id):
+        return mongo.db.ingredients.find_one({"_id": ObjectId(_id)})
+
+    @classmethod
+    def check_vegan(cls, obj):
+        ingredient = cls.find_ingredient_by_id(obj['_id'])
+        return ingredient['vegan']
+
+    @classmethod
+    def check_vegetarian(cls, obj):
+        ingredient = cls.find_ingredient_by_id(obj['_id'])
+        return ingredient['vegetarian']
+
+    @classmethod
+    def check_gluten_free(cls, obj):
+        ingredient = cls.find_ingredient_by_id(obj['_id'])
+        return ingredient['gluten_free']
+
+    @classmethod
+    def check_nut_free(cls, obj):
+        ingredient = cls.find_ingredient_by_id(obj['_id'])
+        return ingredient['nut_free']
+
+    @classmethod
+    def check_egg_free(cls, obj):
+        ingredient = cls.find_ingredient_by_id(obj['_id'])
+        return ingredient['egg_free']
+
--- code/resources/recipes.py
+++ code/resources/recipes.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from flask import request
 
 from models import mongo
 from models.recipes import RecipesModel
@@ -18,11 +19,11 @@ class Recipe(Resource):
                         required=True,
                         help="Recipe must have a image")
     parser.add_argument('method',
-                        type=list,
+                        action='append',
                         required=True,
                         help="Recipe must have a method")
     parser.add_argument('ingredients',
-                        type=list,
+                        action='append',
                         required=True,
                         help="Recipe must have ingredients")
 
@@ -38,4 +39,44 @@ class RecipeCollection(Resource):
         return {
             'recipes': recipes
         }
+
+    def post(self):
+        request_data = request.json
+        ingredient_info = request_data['ingredients']
+        allergies = {
+            "vegan": True,
+            "vegetarian": True,
+            "gluten_free": True,
+            "nut_free": True,
+            "egg_free": True
+        }
+
+        # itterate through the ingedients & check for allergies
+
+        for ingredient in ingredient_info:
+            if not RecipesModel.check_vegan(ingredient['ingredient']):
+                allergies["vegan"] = False
+            if not RecipesModel.check_vegetarian(ingredient['ingredient']):
+                allergies["vegetarian"] = False
+            if not RecipesModel.check_gluten_free(ingredient['ingredient']):
+                allergies["gluten_free"] = False
+            if not RecipesModel.check_nut_free(ingredient['ingredient']):
+                allergies["nut_free"] = False
+            if not RecipesModel.check_egg_free(ingredient['ingredient']):
+                allergies["egg_free"] = False
+
+        request_data['allergies'] = allergies
+
+        try:
+            mongo.db.recipes.insert_one(request_data)
+            return RecipesModel.return_as_object(request_data)
+        except:
+            return {"message": "An error occurred"}, 500
+
+
+        if RecipesModel.find_by_name(request_data['name']):
+            return {'message': "A recipe with name '{}' already exists".format(request_data['name'])}, 400
+        else:
+            return 'x'
+
 
