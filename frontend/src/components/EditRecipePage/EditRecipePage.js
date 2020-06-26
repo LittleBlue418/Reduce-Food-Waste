15442def0c8ee4a6d7743353ef209b2821301c8c
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-26 22:03:29 +0200

    Delete recipe dialogue box implimented

--- frontend/src/components/EditRecipePage/EditRecipePage.js
+++ frontend/src/components/EditRecipePage/EditRecipePage.js
@@ -10,6 +10,11 @@ import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import IconButton from '@material-ui/core/IconButton';
 
 
@@ -21,6 +26,7 @@ const EditRecipePage = () => {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
   const { recipe_id } = useParams();
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false)
 
   const history = useHistory();
 
@@ -58,16 +64,41 @@ const EditRecipePage = () => {
       history.push('/')
     }).catch((error) => {
       setError(error.response.data.message)
+      setOpenDeleteDialog(false)
     })
   }
 
 
+
   return (
     <div className={classes.EditRecipePage}>
+
+      <Dialog
+        open={openDeleteDialog}
+        onClose={() => setOpenDeleteDialog(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Delete Recipe"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to permanently delete this recipe?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpenDeleteDialog(false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={deleteRecipeFromDatabase} color="primary" >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <div className={classes.HeaderDiv}>
         <Link to={`/recipe/${recipe_id}`}><ArrowBackIcon color="primary" /></Link>
         <h2>Edit Recipe</h2>
-        <IconButton onClick={deleteRecipeFromDatabase}>
+        <IconButton onClick={() => setOpenDeleteDialog(true)}>
           <DeleteIcon color="error" />
         </IconButton>
       </div>
