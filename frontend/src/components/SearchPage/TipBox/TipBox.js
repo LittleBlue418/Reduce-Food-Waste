3b587f7c5bec4e3c2456a6e880ac07f3df7d71f9
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-30 12:40:27 +0200

    Spell checking tips

--- frontend/src/components/SearchPage/TipBox/TipBox.js
+++ frontend/src/components/SearchPage/TipBox/TipBox.js
@@ -6,9 +6,12 @@ import Chevron from '../../../assets/icons/chevron-down-solid.svg'
 const tips = [
   {message: "If you cook too much rice put the leftover into the fridge straight away to prevent bacteria forming. Never eat rice that's been left out."},
   {message: "You can re-cook leftover rice, but make sure you heat it thoroughly to kill off any harmful bacteria."},
-  {message: "You can eat cold leftover rice as long as it was refrigerated straight way and not left out."},
+  {message: "You can eat cold leftover rice as long as it was refrigerated straight away and not left out."},
   {message: "Buy one get one free? Put the second one in the freezer for another day!"},
-  {message: "Buying a large peice of meat and cutting it into portions is often cheaper than buying pre-cut. Just freeze the rest for another day."}
+  {message: "Buying a large piece of meat and cutting it into portions is often cheaper than buying pre-cut. Just freeze the rest for another day."},
+  {message: "Have fruit that's bruised? Cut up and freeze the good bits to use in a crumble later on! "},
+  {message: "Find yourself regularly cooking too much? Use scales to measure portions sized before cooking."},
+  {message: "Made a big pot of food? Freeze in small portions for convenient dinners on the go! "}
 ]
 
 const randomNumber = (min, max) => {
